097153e311c638a05f5537f641c723b5f4198764
Fabricio de Sousa Soares <fabriciobb09@gmail.com>
2025-08-03 01:20:29 +0000

    Como faço para deslogar através de um url

---
+++ src/app/logout/route.ts
@@ -0,0 +1,12 @@
+import { NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { logoutAction } from '@/app/actions/authActions';
+
+export async function GET(request: NextRequest) {
+  await logoutAction();
+  
+  // Redirect to the login page after logging out
+  const url = request.nextUrl.clone();
+  url.pathname = '/login';
+  return NextResponse.redirect(url);
+}
